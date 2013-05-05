19157508764e99ce2fd93e3dcb04d9d7bb453520
Christoffer Cena <chris+github@cenaonline.net>
2013-05-05 22:19:09 +0200

    Added active indicator

---
+++ Sluttprosjekt/Helpers/BooleanToActiveStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
+using Sluttprosjekt.Design;
+
+namespace Sluttprosjekt.Helpers
+{
+    public class BooleanToActiveStringConverter : IValueConverter
+    {
+		const string _active = "valgt";
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? _active : "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (string)value == _active;
+        }
+    }
+}
