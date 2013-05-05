7ab85ea26e4cbe5fae79a7d285b8c2f557b4acd7
Christoffer Cena <chris+github@cenaonline.net>
2013-05-05 22:05:54 +0200

    Some more GUI refinement
    Added explanatory text.

---
+++ Sluttprosjekt/Helpers/DecimalToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Sluttprosjekt.Helpers
+{
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((decimal)value == 0) ? "" : ((decimal)value < 0) ? "til gode" : "å betale";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
